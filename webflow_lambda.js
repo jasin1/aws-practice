83e7e88b402848342e4ab6357ac6a37915ef4466
jasin1 <jasin.tairaidrissi@gmail.com>
2023-04-06 15:03:24 +0200

    adding code for lambda

--- webflow_lambda.js
+++ webflow_lambda.js
@@ -1,1 +1,19 @@
 console.log("Start AWS practice");
+
+//----------- First lambda action ever ---------------------//
+const form = document.querySelector('#formData');
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const response = await fetch('arn:aws:lambda:eu-north-1:984112902435:function:helloWorld', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      price: formData.get('price')
+    })
+  });
+  const responseBody = await response.json();
+  const responseElement = document.querySelector('.response');
+  responseElement.innerText = responseBody.message;
+});
